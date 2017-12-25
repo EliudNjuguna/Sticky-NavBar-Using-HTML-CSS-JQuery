1e56a2e0be46e0ccad57e8b967a36c6a0a3a85ed
Eliud Njuguna Kamau <njugunael@gmail.com>
2017-12-25 22:27:31 +0300

    initial commit
    Sticky NavBar using HTML,CSS & JQuery

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).on('scroll',function(){
+       if($(window).scrollTop()){
+           $('nav').addClass('black');
+       } else{
+           $('nav').removeClass('black');
+       }
+    });
+});
